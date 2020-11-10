d0e3187c034d43bdb8c6a93ec470e005b4bd061c
Scott Williams <tocsoft@gmail.com>
2020-11-10 10:30:36 +0000

    add project template

---
+++ src/Templates/templates/PluginTemplate/MyActionHandler.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
+using Tocsoft.StreamDeck;
+
+namespace PluginTemplate
+{
+    public class MyActionHandler
+    {
+        private readonly IActionManager<MyActionSettings> manager;
+
+        public MyActionHandler(IActionManager<MyActionSettings> manager)
+        {
+            this.manager = manager;
+        }
+
+        public Task OnSettingsChanged(MyActionSettings settings)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task OnSendToPlugin(JToken settings)
+        {
+            return Task.CompletedTask;
+        }
+
+        public async Task OnKeyUpAsync()
+        {
+            await manager.ShowOkAsync();
+        }
+
+        public class MyActionSettings
+        {
+            public string Value1 { get; set; }
+            public string Value2 { get; set; }
+        }
+    }
+}
---
+++ src/Templates/templates/PluginTemplate/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using Tocsoft.StreamDeck;
+
+namespace PluginTemplate
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureStreamDeck(args, c =>
+                {
+                    c.AddAction<MyActionHandler>(a =>
+                    {
+                        a.Name = "My Action";
+                    });
+                });
+    }
+}
